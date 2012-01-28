c55b61ed9ecdc830d7e224c75f9633ea7c75da84
Ben Fields <me@benfields.net>
2012-01-28 17:34:41 +0100

    grabs cities from semetric api, with filters

--- src/cities.py
+++ src/cities.py
@@ -14,17 +14,36 @@ Copyright (c) 2012
 import sys
 import os
 import unittest
-import crentials #api keys in here
+import urllib2
+from simplejson import loads
+import credentials #api keys in here
 
 class Cities:
-	def __init__(self):
-		pass
-
+    def __init__(self, artist):
+        self.artist = artist
+        self.by_torrent = None
+    
+    def get_top_by_torrent(self, country=None, continent=None):
+        """
+        returns the top cities by torrent, fetch them from semetric if not already cached
+        can filter the country or continent by any iso code (eg "GB" for the United Kingdom or "EU" for Europe)
+        """
+        if not self.by_torrent:
+            uri = "http://api.semetric.com/artist/{artistID}/downloads/bittorrent/location/city?token={key}"
+            self.by_torrent = loads(urllib2.urlopen(\
+                uri.format(artistID=self.artist,
+                         key=credentials.semetric_api)).read())['response']['data']
+        cities = self.by_torrent
+        if country:
+            cities = [city for city in cities if city['city']['region']['country']['code']==country]
+        elif continent:
+            cities = [city for city in cities if city['city']['region']['continent']['code']==continent]
+        return cities 
 
 class citiesTests(unittest.TestCase):
-	def setUp(self):
-		pass
+    def setUp(self):
+        pass
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
