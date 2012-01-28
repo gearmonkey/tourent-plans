6317d7ef0b04c690d11158d42a2756dbdb453a98
Ben Fields <me@benfields.net>
2012-01-28 16:54:50 +0100

    cities class added

---
+++ src/cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+cities.py
+
+Grabs city data (torrents from the semetric api) for an arbitrary artist (via and id method)
+
+optionally filters by country or continent
+
+Created by Benjamin Fields on 2012-01-28.
+Copyright (c) 2012
+"""
+
+import sys
+import os
+import unittest
+import crentials #api keys in here
+
+class Cities:
+	def __init__(self):
+		pass
+
+
+class citiesTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
